c2d310234ee0bc2a9351e8dbd55d9f662d56fe5d
Yudu <yudu@douyu.tv>
2018-03-21 22:18:59 -0700

    Initial Commit

---
+++ Template/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Template
+//
+//  Created by yu on 2018/3/21.
+//  Copyright © 2018年 yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Template/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Template
+//
+//  Created by yu on 2018/3/21.
+//  Copyright © 2018年 yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
